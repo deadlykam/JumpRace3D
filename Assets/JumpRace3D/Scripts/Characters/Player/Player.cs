35146838a134109c8003e40d3b0a8b9b4bba3706
Kamran Wali <kamran369mars@gmail.com>
2020-05-29 15:03:55 +0100

    Created Player
    # Player
    #### Details:
    Created the Player class. For now the player can move forward.
    ***

---
+++ Assets/JumpRace3D/Scripts/Characters/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class <c>Player</c> handles all the player related functions.
+/// </summary>
+public class Player : BasicAnimation
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBasicAnimation(); // Calling the animation update
+        HorizontalMovement();   // Making player go forward
+    }
+
+    /// <summary>
+    /// This method makes the player go forward when triggered by
+    /// the user.
+    /// </summary>
+    protected override void HorizontalMovement()
+    {
+        // Condition for moving forward
+        if (Input.GetMouseButton(0)) base.HorizontalMovement();
+    }
+}
