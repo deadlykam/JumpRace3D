27e3a43d2f0e29b166a73c7b79bb1657cebf2a81
Kamran Wali <kamran369mars@gmail.com>
2020-05-29 18:42:29 +0100

    Update BouncyStage.cs
    #BouncyStage
    #### Details
    Added method to hide/show the booster.
    ***

--- Assets/JumpRace3D/Scripts/Obstacles/BouncyStage.cs
+++ Assets/JumpRace3D/Scripts/Obstacles/BouncyStage.cs
@@ -6,7 +6,17 @@ public class BouncyStage : MonoBehaviour
 {
     [Header("Bouncy Stage Properties")]
 
-    [Tooltip("This is the position of linked stage which is needed" +
+    [Tooltip("[depricated]This is the position of linked stage which is needed" +
              " for character direction calculation")]
     public Vector3 LinkedStage; // The position of the linked stage
+
+    public GameObject Booster;
+
+    /// <summary>
+    /// This method hides/shows the booster.
+    /// </summary>
+    /// <param name="active">This flag hides or shows the booster,
+    ///                      true = show, false = hide,
+    ///                      of type bool</param>
+    public void SetBooster(bool active) { Booster.SetActive(active); }
 }
