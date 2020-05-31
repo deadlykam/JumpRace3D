7e06c4fa1bfdc3fdfaa7fd6965cf3376b135ecf4
Kamran Wali <kamran369mars@gmail.com>
2020-05-31 16:31:59 +0100

    Re-created BasicStage
    # BasicStage
    #### Details:
    This class contains the basic funtionality of a stage. But will be removed again in future. Needed this for some other testing but is not needed anymore. If not needed by next few commit then it will be removed.
    ***

---
+++ Assets/JumpRace3D/Scripts/Obstacles/BasicStage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// [depricated] Class <c>BasicStage</c> handles all the common functinality between all
+/// stages.
+/// </summary>
+public class BasicStage : MonoBehaviour
+{
+    private BasicStage _linkedStage; // The reference of the linked stage
+    public BasicStage LinkedStage
+    {
+        get { return _linkedStage; }
+        set { _linkedStage = value; }
+    }
+
+    public Vector3 LinkedStagePosition // The position of the linked stage
+    {
+        get { return _linkedStage.transform.position; }
+    }
+}
