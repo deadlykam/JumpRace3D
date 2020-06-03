b1ceecfa50f390c8206f7c84953b553457f7cde4
Kamran Wali <kamran369mars@gmail.com>
2020-06-03 11:33:16 +0100

    Created GameData
    # GameData
    #### Details:
    This class is responsible for handling game global data. For now it has **float FallHeightLimit**. In future it will contain fields and functionality for simulation speed and saving and loading game data.
    ***

---
+++ Assets/JumpRace3D/Scripts/Others/GameData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class <c>GameData</c> contains data that will be used by other classes.
+/// </summary>
+public class GameData : MonoBehaviour
+{
+    [Tooltip("The y-axis value limit for objects to fall to.")]
+    public float FallHeightLimit; // The limit of fall distance
+
+    // TODO: Give fields for simulation speed
+
+    public static GameData Instance;
+
+    void Awake()
+    {
+        if (Instance == null) // NOT initialized
+        {
+            Instance = this; // Initializing the instance
+
+            DontDestroyOnLoad(gameObject); // Making it available
+                                           // throughout the game
+        }
+        else Destroy(gameObject); // Already initialized and
+                                  // destroying duplicate
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
