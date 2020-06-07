500b0d3f343de1407b411e3348f915dec2435c2d
Kamran Wali <kamran369mars@gmail.com>
2020-06-07 22:41:22 +0100

    Update BasicAnimation.cs
    # Ragdoll Logics
    #### Details:
    Added logic to store and trigger ragdoll. The method **void SetRagdoll(bool)** activates and deactivates the ragdoll. Please keep testing and report any bugs.
    ***

--- Assets/JumpRace3D/Scripts/Characters/BasicAnimation.cs
+++ Assets/JumpRace3D/Scripts/Characters/BasicAnimation.cs
@@ -13,11 +13,23 @@ public class BasicAnimation : BasicCharacter
     private Animator _characterAnimator; // The character animations
 
     [SerializeField]
+    private Transform _model; // The animation model
+
+    [SerializeField]
+    private Transform _ragdollModel; // The ragdoll model
+
+    // All children transform in the model
+    private List<Transform> _modelChildren = new List<Transform>();
+
+    // All children transform in the ragdoll model
+    private List<Transform> _ragdollModelChildren = new List<Transform>();
+
+    [SerializeField]
     private int _fallAnimations; // Number of fall animations
 
     [SerializeField]
     private int _jumpAnimations; // Number of jump animations
-
+    
     private string _fallSelectParameter = "FallSelect";
     private string _jumpSelectParameter = "JumpSelect";
     private string _triggerJumpParameter = "TriggerJump";
@@ -26,7 +38,7 @@ public class BasicAnimation : BasicCharacter
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupRagDoll();
     }
     
     /// <summary>
@@ -49,7 +61,67 @@ public class BasicAnimation : BasicCharacter
         UpdateBasicCharacter(); // Calling Basic Character update
         UpdateAnimationSpeed(); // Updating the animation speed
     }
+
+    /// <summary>
+    /// This method sets up the ragdoll children.
+    /// </summary>
+    protected void SetupRagDoll()
+    {
+        // Loop for adding all model children
+        foreach(Transform child in _model.GetComponentsInChildren<Transform>())
+        {
+            _modelChildren.Add(child);
+        }
+
+        // Loop for adding all ragdoll children
+        foreach (Transform child in _ragdollModel.GetComponentsInChildren<Transform>())
+        {
+            _ragdollModelChildren.Add(child);
+        }
+    }
     
+    /// <summary>
+    /// This method starts or stops the ragdoll of a character.
+    /// </summary>
+    /// <param name="active">The flag to activate/deactivate a ragdoll,
+    ///                      <para>true = activate ragdoll</para>
+    ///                      <para>false = deactivate ragdoll</para>
+    ///                      of type bool</param>
+    protected void SetRagdoll(bool active)
+    {
+        if (active) // Condition for activating ragdoll
+        {
+            // Loop for making the ragdoll match the animation from model
+            for(int i = 0; i < _modelChildren.Count; i++)
+            {
+                // Setting ragdoll position to animation position
+                _ragdollModelChildren[i].position 
+                    = _modelChildren[i].position;
+
+                // Setting ragdoll rotation to animation rotatin
+                _ragdollModelChildren[i].rotation 
+                    = _modelChildren[i].rotation;
+
+                // Checking if the ragdoll child has rigidbody
+                if(_ragdollModelChildren[i]
+                    .GetComponent<Rigidbody>() != null)
+                {
+                    // Removing any velocities from the ragdoll child
+                    _ragdollModelChildren[i].GetComponent<Rigidbody>()
+                        .velocity = Vector3.zero;
+                }
+            }
+        }
+
+        _ragdollModel.gameObject.SetActive(active); // Activate/deactivate
+                                                    // ragdoll
+
+        _model.gameObject.SetActive(!active); // Activate/deactivate model
+
+        mainCollider.enabled = !active; // Activate/deactivate
+                                        // main collider
+    }
+
     /// <summary>
     /// This method plays jump animation of the character and selects
     /// a random fall animation as well.
@@ -115,6 +187,8 @@ public class BasicAnimation : BasicCharacter
     public override void StartCharacter()
     {
         base.StartCharacter();
+
+        SetRagdoll(false); // Resetting the ragdoll <-- Should not be here
 
         FallAnimation(); // Playing a fall animation
     }
