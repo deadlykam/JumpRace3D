0ac34ad60956cf89766715a4a4ca2c37bae3a66f
Kamran Wali <kamran369mars@gmail.com>
2020-06-10 19:00:51 +0100

    Removed commented out codes

--- Assets/JumpRace3D/Scripts/Characters/Player/Player.cs
+++ Assets/JumpRace3D/Scripts/Characters/Player/Player.cs
@@ -30,12 +30,6 @@ public class Player : BasicAnimation
                                   // destroying duplicate
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // SetupRagDoll(); // Setting up the ragdoll colliders
-    }
-
     // Update is called once per frame
     void Update()
     {
