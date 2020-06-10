2c777665416a473bc0286cd0082cff4463b546a9
Kamran Wali <kamran369mars@gmail.com>
2020-06-10 17:31:08 +0100

    Update CharacterInfo.cs
    # Added model, ragdoll and animation
    #### Details:
    This class now holds the model, ragdoll and animation of a character model. This way different character models can be assigned to different characters.
    ***

--- Assets/JumpRace3D/Scripts/Characters/CharacterInfo.cs
+++ Assets/JumpRace3D/Scripts/Characters/CharacterInfo.cs
@@ -6,12 +6,35 @@ public class CharacterInfo : MonoBehaviour
 {
     [Header("Character Info Properties")]
     [SerializeField]
+    private Transform _model; // The character model
+
+    /// <summary>
+    /// Returns the model object, of type Transform
+    /// </summary>
+    public Transform Model { get { return _model; } }
+
+    [SerializeField]
     private Transform _ragdollModel; // The ragdoll associated
                                      // with this model
+
+    /// <summary>
+    /// Returns the ragdoll model object, of type Transform
+    /// </summary>
+    public Transform RagdollModel { get { return _ragdollModel; } }
 
     [SerializeField]
     private Transform _headBone; // The location of the
                                  // head bone
+
+    [SerializeField]
+    private Animator _characterAnimator; // The animator of the
+                                         // character
+
+    /// <summary>
+    /// Returns the animator of the character, of type Animator
+    /// </summary>
+    public Animator CharacterAnimator
+    { get { return _characterAnimator; } }
 
     /// <summary>
     /// This method sets the crown on the character model.
