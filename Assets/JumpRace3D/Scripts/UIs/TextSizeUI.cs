eae251254b0d1bb629756979ba2e1c83be4812ec
Kamran Wali <kamran369mars@gmail.com>
2020-06-11 23:52:30 +0100

    Update TextSizeUI.cs
    # Field privacy
    #### Details:
    Changed privacy of some fields which is needed by the PopupUI.
    ***

--- Assets/JumpRace3D/Scripts/UIs/TextSizeUI.cs
+++ Assets/JumpRace3D/Scripts/UIs/TextSizeUI.cs
@@ -7,7 +7,12 @@ public class TextSizeUI : BasicUISpeedEffect
 {
     [Header("Text Size UI Properties")]
     [SerializeField]
-    private TextMeshProUGUI Text; // Text UI to change size of
+    private TextMeshProUGUI _text; // Text UI to change size of
+
+    /// <summary>
+    /// Returns the Text, of type TextMeshProUGUI
+    /// </summary>
+    protected TextMeshProUGUI text { get { return _text; } }
 
     [SerializeField]
     [Tooltip("The minimum size of the text in percentage.")]
@@ -17,12 +22,22 @@ public class TextSizeUI : BasicUISpeedEffect
 
     private float _sizeMax; // Maximum text size
 
+    /// <summary>
+    /// The maximum size of the text font, of type float
+    /// </summary>
+    protected float sizeMax { get { return _sizeMax; } }
+
     private float _sizeMin; // Minimum text size
+
+    /// <summary>
+    /// The minimum size of the text font, of type float
+    /// </summary>
+    protected float sizeMin { get { return _sizeMin; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        _sizeMax = Text.fontSize; // Storing the max font size
+        _sizeMax = _text.fontSize; // Storing the max font size
         _sizeMin = _sizeMax * _sizePercentage; // Storing the min
                                                // font size
     }
@@ -33,6 +48,6 @@ public class TextSizeUI : BasicUISpeedEffect
         UpdateBasicUISpeedEffect(); // Updating BasicUISpeedEffect
 
         // Changing the size of the font
-        Text.fontSizeMax = Mathf.Lerp(_sizeMin, _sizeMax, step);
+        _text.fontSizeMax = Mathf.Lerp(_sizeMin, _sizeMax, step);
     }
 }
