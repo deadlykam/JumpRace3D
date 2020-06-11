677f661cb2b441e7578b92e2d5c0e087ba186732
Kamran Wali <kamran369mars@gmail.com>
2020-06-11 16:53:32 +0100

    Created SideMoveUI
    # SideMoveUI
    #### Details:
    This class moves the UI element from _leftTarget to _rightTarget.
    ***

---
+++ Assets/JumpRace3D/Scripts/UIs/SideMoveUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideMoveUI : BasicUISpeedEffect
+{
+    [Header("Side Move UI Properties")]
+    [SerializeField]
+    private Transform _leftTarget; // Left target
+
+    [SerializeField]
+    private Transform _rightTarget; // Right target
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        UpdateBasicUISpeedEffect(); // Updating BasicUISpeedEffect
+
+        // Moving the UI
+        transform.position = Vector3.Lerp(_leftTarget.position, 
+                                          _rightTarget.position, 
+                                          step);
+    }
+}
