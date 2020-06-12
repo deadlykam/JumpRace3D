21aaa1fb30b715fcd8d478dbbc46b88a79ec7a02
Kamran Wali <kamran369mars@gmail.com>
2020-06-12 19:29:34 +0100

    Update RaceTracker.cs
    # Complete structure change
    #### Details:
    Removed the process way of finding the leader and race position of other racers. This class now sorts all the racers position in the game. It also shows who is what position in the in-game position visual. This is now only called by the Player class.
    ***

--- Assets/JumpRace3D/Scripts/Others/RaceTracker.cs
+++ Assets/JumpRace3D/Scripts/Others/RaceTracker.cs
@@ -7,32 +7,28 @@ public class RaceTracker : MonoBehaviour
     [Header("Race Tracker Properties")]
     [SerializeField]
     private Transform _leaderCrown;  // The leader crown object
-
-    [SerializeField]
-    private Vector3 _crownOffset; // The offset value of the crown
-                                  // when placed inside a racer
-
-    private int _leaderRacePosition = -1; // The current position 
-                                          // of the leader in a 
-                                          // race
-
-    // Storing all the race position requests
-    private List<RacePositionRequest> _racePositionRequests 
-        = new List<RacePositionRequest>();
-
-    private RacePositionRequest _currentRequest; // Current race
-                                                 // position
-                                                 // request
-
-    /// <summary>
-    /// For checking if any requests are available, of type bool
-    /// </summary>
-    private bool _isRequest
-    { get { return _racePositionRequests.Count != 0; } }
-
+                                          
     private bool _isProcessing = false; // Flag to check if a 
                                         // request is being
                                         // processed
+
+    // List of all the racers in the game playing
+    private List<BasicCharacter> _racers = new List<BasicCharacter>();
+
+    // Converting the racers list to array
+    private BasicCharacter[] _racersToArray;
+
+    private int _indexCompare; // Index for comparing a character
+
+    private int _indexSearch; // Index for searching a character
+                              // to compare to
+
+    private bool _isPlayerShown; // Bool to check if player position
+                                 // has been shown or not
+
+    // Storing the character being compared which will be
+    // needed for swapping
+    private BasicCharacter _currentCharacter;
 
     public static RaceTracker Instance; // Singleton
 
@@ -59,57 +55,135 @@ public class RaceTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_isRequest) // Condition to check if any request available
+        // Condition to prcess the raceer positions
+        if (_isProcessing) ProcessPosition();
+    }
+    
+    /// <summary>
+    /// This method processes the racers positions.
+    /// </summary>
+    private void ProcessPosition()
+    {
+        _indexCompare = 1; // Starting compare index
+        _currentCharacter = null; // Resetting current character
+        _isPlayerShown = false; // Player position has not
+                                // been shown
+
+        // Loop for going through all the characters
+        while(_indexCompare < _racersToArray.Length)
         {
-            if (!_isProcessing) // Checking if any process going on
+            // Storing current character for comparison
+            _currentCharacter = _racersToArray[_indexCompare];
+
+            _indexSearch = _indexCompare - 1; // Getting starting
+                                              // index for compare
+
+            // Loop for comparing and checking if the character
+            // is less than the current character
+            while(_indexSearch >= 0 &&
+                  _racersToArray[_indexSearch]
+                  .CompareTo(_currentCharacter) == -1)
             {
-                _isProcessing = true; // Starting process
-                ProcessRequest();     // Processing reqest
+                // Swaping the characters
+                _racersToArray[_indexSearch + 1] 
+                    = _racersToArray[_indexSearch];
+
+                _indexSearch--; // Getting another character to
+                                // compare to
             }
+
+            // Putting current character at the right order
+            _racersToArray[_indexSearch + 1] = _currentCharacter;
+
+            _indexCompare++; // Getting another character to
+                             // compare to
         }
-    }
 
-    /// <summary>
-    /// This method processes the request and sets up the leader and
-    /// it's crown.
-    /// </summary>
-    private void ProcessRequest()
-    {
-        // Storing the first request
-        _currentRequest = _racePositionRequests[0];
-
-        // Removing the first request
-        _racePositionRequests.RemoveAt(0);
-
-        // Condition to check if a new leader emerged from the
-        // race
-        if(_leaderRacePosition == -1 || 
-           _currentRequest.RacePosition < _leaderRacePosition)
+        // Checking if models has been assigned to the character
+        if (_racersToArray[0].ModelInfo != null)
         {
-            // Setting the new leader position
-            _leaderRacePosition = _currentRequest.RacePosition;
-
             // Setting the crown on a new leader
-            _currentRequest.RacerModel
-                .SetCrown(_leaderCrown, _crownOffset);
+            _racersToArray[0].ModelInfo.SetCrown(_leaderCrown);
         }
 
-        _isProcessing = false; // Process done
+        // Loop to show the first 3 character positions including the player
+        for(int i = 0; i < _racersToArray.Length; i++)
+        {
+            // Condition to show first 2 positions
+            if (i < 2)
+            {
+                // Checking if the player is in first 2 positions
+                if(!_isPlayerShown &&
+                    _racersToArray[i].CharacterName == 
+                    GameData.Instance.PlayerName)
+                {
+                    MainCanvasUI.Instance
+                        .SetInGameRacePosition(i, true, 
+                        _racersToArray[i].CharacterName);
+
+                    _isPlayerShown = true;
+                }
+                else // Showing other characters positions
+                {
+                    //TODO: Show character's name
+                    MainCanvasUI.Instance
+                        .SetInGameRacePosition(i, false,
+                        _racersToArray[i].CharacterName);
+                }
+            }
+            // Condition for showing the player's position if NOT shown
+            else if (!_isPlayerShown)
+            {
+                // Finding the player's character
+                if (_racersToArray[i].CharacterName ==
+                    GameData.Instance.PlayerName)
+                {
+                    //TODO: Show player's name
+                    MainCanvasUI.Instance
+                        .SetInGameRacePosition(i, true,
+                        _racersToArray[i].CharacterName);
+
+                    break;
+                }
+            }
+            // Condition for already showing the player's position
+            // and now needs to show 1 more character position
+            else
+            {
+                //TODO: Show character's name
+                MainCanvasUI.Instance
+                        .SetInGameRacePosition(i, false,
+                        _racersToArray[i].CharacterName);
+
+                break;
+            }
+            
+        }
+
+        _isProcessing = false; // Processing done
     }
 
     /// <summary>
-    /// This method adds a new request for race position.
+    /// This method starts the race position process
     /// </summary>
-    /// <param name="racePosition">The position of the racer, of type
-    ///                            int</param>
-    /// <param name="racerModel">The model of the racer, of type
-    ///                          CharacterInfo</param>
-    public void AddRequest(int racePosition, CharacterInfo racerModel)
+    public void UpdateRacePosition() { _isProcessing = true; }
+
+    /// <summary>
+    /// This method adds a racer to the racer list.
+    /// </summary>
+    /// <param name="racer">The racer to add, 
+    ///                     of type BasicCharacter</param>
+    public void AddRacer(BasicCharacter racer) { _racers.Add(racer); }
+
+    /// <summary>
+    /// This method initializes the race tracker at the start of a race.
+    /// </summary>
+    public void StartRaceTracker()
     {
-        // Creating a new request and adding it to the request list
-        _racePositionRequests.Add(new RacePositionRequest(
-                                    racePosition,
-                                    racerModel));
+        // Converting all the racers list to array
+        _racersToArray = _racers.ToArray();
+
+        UpdateRacePosition(); // Showing the correct position at start
     }
 
     /// <summary>
@@ -121,46 +195,7 @@ public class RaceTracker : MonoBehaviour
         // Hiding the crown
         _leaderCrown.gameObject.SetActive(false);
 
-        // Resetting the leader position
-        _leaderRacePosition = -1;
-
-        // Remvoing all requests from the list
-        _racePositionRequests.Clear();
-    }
-
-    /// <summary>
-    /// This struct creates a race position request.
-    /// </summary>
-    struct RacePositionRequest
-    {
-        private int _racePosition; // The current position of the 
-                                   // racer in a race
-
-        /// <summary>
-        /// The race position of the race, of type int
-        /// </summary>
-        public int RacePosition { get { return _racePosition; } }
-
-        private CharacterInfo _racerModel; // The transform of the
-                                           // racer model if crown
-                                           // needs to be exchanged
-
-        /// <summary>
-        /// The transform of the racer model, of type CharacterInfo
-        /// </summary>
-        public CharacterInfo RacerModel { get { return _racerModel; } }
-
-        /// <summary>
-        /// Constructor for creating a race position request.
-        /// </summary>
-        /// <param name="racePosition">The position of the racer in a
-        ///                            race, of type int</param>
-        /// <param name="racerModel">The model of the racer, of type
-        ///                          CharacterInfo</param>
-        public RacePositionRequest(int racePosition, CharacterInfo racerModel)
-        {
-            _racePosition = racePosition;
-            _racerModel = racerModel;
-        }
+        // Removing all the racers from the list
+        _racers.Clear();
     }
 }
